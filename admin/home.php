dd9bf7593f9ca29139cf8b9dd99d10a188adfa86
quocanh273 <anhnqph32683@fpt.edu.vn>
2023-11-09 19:01:56 +0700

    checkot

--- admin/home.php
+++ admin/home.php
@@ -40,7 +40,7 @@
 			</ul>
 
 
-			<div class="table-data">
+			<!-- <div class="table-data">
 				<div class="order">
 					<div class="head">
 						<h3>Đơn Hàng Gần Đây	</h3>
@@ -100,6 +100,39 @@
 					</table>
 				</div>
 				
+			</div> -->
+
+			<div class="bieudo">
+				
+			<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+
+    <div id="piechart" style="width: 100%; height: 750px;"></div>
+ 
+
 			</div>
 		</main>
         </section>
